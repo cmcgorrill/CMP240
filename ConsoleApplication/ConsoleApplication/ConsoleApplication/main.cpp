a15ca50bff89a96a6d6ab9eb33c73a0af5bb4cf8
Courtney <cmm5215@rit.edu>
2016-09-19 19:45:00 -0400

    lab 2 start

---
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ConsoleApplication
+//
+//  Created by Courtney McGorrill on 9/18/16.
+//  Copyright © 2016 Courtney McGorrill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int i;
+    for(i = 0; i < 10; i++) {
+        printf("Line number %d\n", i);
+    }
+    return 0;
+}
