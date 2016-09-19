55a88af290eec7822402aa5c1bc6747b7351dc20
Courtney <cmm5215@rit.edu>
2016-09-19 13:48:32 -0400

    IT WORKS

--- lab_1/source/main.c
+++ lab_1/source/main.c
@@ -10,15 +10,10 @@
 // Write a function that will delay for n loops
 // to satisfy timing constraints given in lab. 
 // ....beware compiler optimization....
-void delay(uint32_t count)
+void delay(uint32_t volatile count)
 {
 	while  (count>0)
     {
-        int i = 1600;   // This number is equivalent to  ms
-        while (i>0)
-        {
-            i--;
-        }
         count--;
     }
 }
@@ -30,9 +25,9 @@ int main()
     while (1)
     {       
         gpio[GPSET0] = 0x10000;
-        delay(10);
+        delay(3000000);
         gpio[GPCLR0] = 0x10000;
-        delay(10);
+        delay(3000000);
 
     }
     
