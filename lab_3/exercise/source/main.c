05980242aad7a6f2ee7fe175b486bbd6c2ad9325
Courtney <cmm5215@rit.edu>
2016-10-17 08:04:04 -0400

    lab 3 started

---
+++ lab_3/exercise/source/main.c
@@ -0,0 +1,62 @@
+#include "gpio.h"
+#include "system_timer.h"
+#include "uart.h"
+
+void blink_once()
+{
+    // Turn on LED
+    gpio[GPSET0] = 0x10000;
+    timer_delay_sec(1);
+
+    // Turn off LED
+    gpio[GPCLR0] = 0x10000;
+
+}
+
+void blink_code(uint32_t err)
+{
+    for(int i = 0; i < err; ++i)
+    {
+        // Blink the LED
+        blink_once();
+
+    }
+
+    // Delay for desired time
+    timer_delay_sec(5);
+    
+}
+
+int main()
+{
+    // Init GPIO select for external LED
+    gpio[GPFSEL1] = 0x40000;
+
+    // Init uart for debugging purposes
+    init_uart();
+
+    // Provide a buffer size for debug prints
+    const int bufferSize = 80;
+    char buffer [bufferSize];
+
+    // You might need a count for something
+    uint32_t count = 1;
+
+    // Mainline loop
+    while (1)
+    {
+        // implement error code described in lab
+        if(count > 0){
+            blink_code(count);
+        }
+        count++;
+        
+        if(count%10 == 0){
+            count = 0;
+        }
+        
+
+    }
+    
+    return 0;
+}
